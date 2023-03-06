de0c5f910e5e6554b71c57a64bf4b66145e0e847
André Basseto <andre.basseto@hotmail.com>
2023-03-05 23:09:22 -0300

    java doc

---
+++ java-terminal/java-terminal/src/Calculadora.java
@@ -0,0 +1,19 @@
+/**
+ * @author André Basseto
+ * @version 1.0
+ * @since 05/03/2023
+ */
+public class Calculadora {
+    /**
+     * Este método é utilizado para somar dois numeros inteiros
+     * 
+     * @param numeroUm   este é um parametro do metodo
+     * @param numeroDois este é um parametro do metodo
+     * @return int o resultado des metodo é a soma dos dois numeros
+     *
+     */
+
+    public int somar(int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois;
+    }
+}
